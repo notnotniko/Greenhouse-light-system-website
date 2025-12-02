b04ffd345c0b06cd114e279a5e55c351f0888431
notnotniko <mattjaquire47@gmail.com>
2025-12-02 17:12:30 -0500

    Add files via upload

--- greenhouseLightingSystem/greenhouseLightingSystem.ino
+++ greenhouseLightingSystem/greenhouseLightingSystem.ino
@@ -1,7 +1,7 @@
 //Lighting system with Photoresistor (LDR)
 //Author: Patrick Kiernicki
 //Nov 18, 2025
-//If the LDR detects the light level to be under a certain value (400), it will turn the LEDs on.
+//If the LDR detects the light level to be under a certain value, it will turn the LEDs on.
 
 //LDR pin
 int sensorPin = A0;
@@ -13,10 +13,10 @@ void setup() {
 }
 void loop() {
   int lightLevel = analogRead(sensorPin); //measure light level
-  if (lightLevel < 20) {
-    digitalWrite(ledControlPin, HIGH); //turns LEDs on
-  } else {
+  if (lightLevel < 6) {
     digitalWrite(ledControlPin, LOW); //turns LEDs off
+  } else {
+    digitalWrite(ledControlPin, HIGH); //turns LEDs on
 
   }
 }
